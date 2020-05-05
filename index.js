2faefceac80df51dc7df88639d9cbd8223e84f12
Mike Gretton <mikegretton@gmail.com>
2020-05-05 22:29:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ function subtract(a, b) { return a - b; }
 function multiple(a, b) { return a * b; }
 function multiply(a, b) { return a * b; }
 function divide(a, b) { return a / b; }
+function increment(n) { return ++n; }
+function decrement(n) { return --n; }
